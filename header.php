6312429d8a22ecd2bf62c6a63fe730e4a24a7deb
oshoneugiomoh <jugiomoh@gmail.com>
2016-11-06 00:12:43 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "oshoneugiomoh";
+$password = "inspiron1545";</p>
+
+<p>// Create connection
+$conn = new mysqli($localost, $oshoneugiomoh, $inspiron1545);</p>
+
+<p>// Check connection
+if ($conn-&gt;connect_error) {
+    die("Connection failed: " . $conn-&gt;connect_error);
+} 
+echo "Connected successfully";
+?>
